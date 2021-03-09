49b92e9bb2e36303b08a3717222185268f3350c2
nrnfbrnt <nrnfbrnt12@gmail.com>
2021-03-09 22:38:27 +0700

    Get and Post

---
+++ tugas2.php
@@ -0,0 +1,136 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Online Shop</title>
+</head>
+<body>
+    <div class="bg-success p-5">
+    <h1 class="text-center text-white">Online Shop</h1>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6">
+            <form action="tugas4.php" method="post" class="mt-3">
+            
+            <div class="form-group">
+                    <label for=""> Customer </label>
+                    <input type="text" name="customer" value="" class="form-control">
+                </div>
+                <div class="form-group">
+                <label for="">Item</label><br>
+                  <div class="form-check form-check-inline">
+                  
+                    <input class="form-check-input" type="radio" name="item" id="inlineRadio1" value="TV">
+                    <label class="form-check-label" for="inlineRadio1">TV</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="item" id="inlineRadio2" value="Refrigator">
+                    <label class="form-check-label" for="inlineRadio2">Refrigator</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="item" id="inlineRadio3" value="Washing Machine" disabled>
+                    <label class="form-check-label" for="inlineRadio3">Washing Machine</label>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="">Total Item</label>
+                    <input type="text" name="totalItem" value="" class="form-control">
+                </div>
+                
+                <div class="form-group">
+                    <label for="">Date Shipping</label>
+                    <input type="date" name="date" value="" class="form-control">
+                </div>
+
+                <label for="">Courier Type</label>
+                <select class="form-select form-control" class="form-control" name="courierType">
+                    <option selected></option>
+                    <option value="Tiki">Tiki</option>
+                    <option value="SiCepat">SiCepat</option>
+                    <option value="Pos">Pos</option>
+                    <option value="Grab">Grab</option>
+                    <option value="Gojek">Gojek</option>
+                </select>
+                <div class="form-group">
+                    <label for="">Shipping Address</label>
+                    <textarea class="form-control" name="address" id="" cols="30" rows="5"></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="">Courier Cost</label>
+                    <input type="text" name="courierCost" value="" class="form-control">
+                </div>
+
+                
+                <div class="form-group">
+                    <label for="">Assurance Cost</label>
+                    <input type="text" name="assurance" value="" class="form-control">
+                </div>
+                <input type="submit" value="Simpan" name="" class="btn btn-success btn-sm btn-block">
+
+            </form>
+
+            <?php 
+            $nama = $_POST ['customer'];
+            $item_name = $_POST ['item'];
+            $total_item = $_POST ['totalItem'];
+            $date_shipping = $_POST ['date'];
+            $courier_type = $_POST ['courierType'];
+            $courier_cost = $_POST ['courierCost'];
+            $shipping_address = $_POST ['address'];
+            $assurance = $_POST ['assurance'];
+               
+            if ($item_name == 'TV'){
+                $cost = 3000000;
+            }else if ($item_name == 'Refrigator'){
+                $cost = 6000000;
+            } 
+
+            $total_cost= ($total_item * $cost) + $courier_cost + $assurance;
+
+            
+            ?>
+            </div>
+            <div class="col-3 mt-3">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header bg-success text-white">
+                    Result
+                    </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Customer : <?= $nama ?> </li>
+                    <li class="list-group-item">Item : <?= $item_name ?> </li>
+                    <li class="list-group-item">Total Item : <?= $total_item ?> </li>
+                    <li class="list-group-item">Date Shipping :  <?= $date_shipping ?> </li>
+                    <li class="list-group-item">Courier Type : <?= $courier_type ?> </li>
+                    <li class="list-group-item">Shipping Address : <?= $shipping_address ?> </li>
+                    <li class="list-group-item">Courier Cost : <?=  $courier_cost ?></li>
+                    <li class="list-group-item">Assurance Cost :  <?= $assurance ?></li>
+                    <li class="list-group-item">Total Cost : <?= $total_cost ?> </li>
+                   
+                </ul>
+                </div>
+            </div>
+            <div class="col-3 mt-3">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-header bg-success text-white">
+                    Cost List
+                    </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">TV : Rp.3000.000</li>
+                    <li class="list-group-item">Refrigator : Rp.6000.000</li>
+                    <li class="list-group-item">Washing Machine : Rp. 4000.000</li>
+                </ul>
+                </div>
+            </div>
+            
+        </div>
+        
+    </div>
+</body>
+</html>
